181de35af47e70eba74c249ba0cb2e2d84f4c6d5
minnie <minnie0175@naver.com>
2023-10-17 16:35:59 +0900

    Second commit

--- Homework1(1)/app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ Homework1(1)/app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Windows-10.0.18363")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19045")
 set(CMAKE_HOST_SYSTEM_NAME "Windows")
-set(CMAKE_HOST_SYSTEM_VERSION "10.0.18363")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19045")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
 include("C:/Users/minnie/AppData/Local/Android/Sdk/ndk/19.2.5345600/build/cmake/android.toolchain.cmake")
--- Homework1(1)/app/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
+++ Homework1(1)/app/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/minnie/Desktop/Homework1(1)/app
+# Install script for directory: C:/Users/minnie/Desktop/Computer Graphics Assignment/Homework1(1)/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/minnie/Desktop/Homework1(1)/app/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/minnie/Desktop/Computer Graphics Assignment/Homework1(1)/app/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
